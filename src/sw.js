7a52f230350c571bcfb728ed0398f9ef1f511754
Cris Amarante <amarante_s@hotmail.com>
2026-07-18 15:50:43 -0300

    Update CACHE_NAME for testing purposes

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'penso-cache-v3.1.0.0.1.4';
+const CACHE_NAME = 'pensoTestes-cache-v3.1.0.0.0.0';
 //correção do modal de inspeções
 // Lista de arquivos para cache imediato (estáticos)
 const ASSETS_TO_CACHE = [
